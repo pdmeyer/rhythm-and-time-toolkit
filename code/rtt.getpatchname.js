9d0f5928e11dada2d67c8073757f2496ab733040
pdmeyer <44841504+pdmeyer@users.noreply.github.com>
2023-06-10 12:22:40 -0700

    fixed issue with automatic scripting name

--- code/rtt.getpatchname.js
+++ code/rtt.getpatchname.js
@@ -6,7 +6,7 @@ var p = this.patcher;
 function getname() {
 	var name;
 	var tn = p.name;
-	if(tn && tn != "seq.xtra.pattrhelper") name = tn;
+	if(tn && tn != "rtt.xtra.pattrhelper") name = tn;
 	else if(p.parentpatcher.name) name = p.parentpatcher.name;
 	outlet(0, name);
 	post("pattrstorage name: " + name + "\n")
